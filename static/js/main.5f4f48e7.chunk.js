5a62f2ada003fcea98a7f65c044fba4c78c7bbc4
dantes321 <alexkardash97@gmail.com>
2021-05-18 20:49:39 +0300

    Updates

--- static/js/main.5f4f48e7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpstore=this.webpackJsonpstore||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),o=n.n(s),i=(n(8),n(9),n(0)),a=function(){return Object(i.jsx)("div",{className:"app-wrapper",children:"123123"})};o.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.5f4f48e7.chunk.js.map
