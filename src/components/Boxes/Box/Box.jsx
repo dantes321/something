4bd770ca4824033ac6ecb248a30ed87bf2954a32
dantes321 <alexkardash97@gmail.com>
2021-05-18 18:23:01 +0300

    trying to commit811

--- src/components/Boxes/Box/Box.jsx
+++ src/components/Boxes/Box/Box.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import s from './Box.module.scss'
 const Box = ({price,title,id,img,description}) => {
-  console.log(img);
   return <div className={s.box}>
     <img src={img}/>
     <div className={s.title}>{title}</div>
-    <div className={s.price}>{title}</div>
+    <div className={s.price}>{price}</div>
     <a href='#'>Заказать</a>
   </div>;
 };
